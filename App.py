bab509145eb896abd3ab0b1fa7191737524eb3ba
Aryan2327 <achaudh9@binghamton.edu>
2022-05-01 01:33:15 -0400

    budget vs gdp

--- App.py
+++ App.py
@@ -1,22 +1,164 @@
 import pymongo
 import csv
 import json
+# import seaborn as sns
+import matplotlib.pyplot as plt
 from pymongo import MongoClient
 
+
 def insert(collection):
-    with open('movies.csv', mode = 'r', encoding='utf-8') as csv_file:
+    with open('movies.csv', mode='r', encoding='utf-8') as csv_file:
         dict_objs = csv.DictReader(csv_file)
         id = 1
         for d in dict_objs:
             d["_id"] = id
-            #json_obj = json.dumps(d)
+            # json_obj = json.dumps(d)
             collection.insert_one(d)
             id += 1
 
 
+"""
+Use MongoDB's aggregration pipeline to compute the most popular/acclaimed movies based on genre.
+Provide a data visualization indicating the disparity of genres among popular/acclaimed films.
+"""
+
+
+def topPopularGenres(collection):
+    collection.update_many({"$or": [{"score": ""}, {"votes": ""}]}, {"$set": {"score": 0, "votes": 0}})
+    intConversion = {
+        "$addFields": {
+            "convertedScore": {"$toDouble": "$score"},
+            "convertedVotes": {"$toDouble": "$votes"},
+        }
+    }
+
+    project = {
+        "$project": {"_id": 0, "name": 1, "genre": 1, "metric": {"$multiply": ["$convertedScore", "$convertedVotes"]}},
+    }
+
+    sort = {
+        "$sort": {"metric": -1}
+    }
+
+    limit = {"$limit": 300}
+
+    group = {"$group": {"_id": "$genre", "total_amount": {"$sum": 1}}}
+
+    result = collection.aggregate(
+        [
+            intConversion,
+            project,
+            sort,
+            limit,
+            group
+        ]
+    )
+    # cursor = collection.find().sort({"score": -1}).limit(15)
+
+    genres = []
+    totals = []
+
+    for json in result:
+        print(json)
+        genres.append(json["_id"])
+        totals.append(json["total_amount"])
+
+    # Based on official documentation of matplotlib.
+    figure = plt.figure(figsize=(20, 10))
+
+    plt.bar(genres, totals, color='blue', width=0.4)
+
+    plt.xlabel("Genres")
+    plt.ylabel("Amount of movies")
+    plt.title("Top Genres in Top 300 Most Popular Movies")
+    plt.show()
+
+
+"""
+Utilize MongoDB querying to access various collections from the entire database of movie information.
+Visualize the relationship between movie budget-revenue ratio and the country of the movie's production GDP. 
+We also provide a model via simple linear regression.
+"""
+
+
+def budgetRevenueRelationship(collection1, collection2):
+    # result = collection1.aggregate(
+    #     [{"$group": {"_id": "$country", "total_amount": {"$sum": 1}}}]
+    # )
+    # for i in result:
+    #     print(i)
+
+    remove_whitespace = {
+        "$addFields": {
+            "country": {"$trim": {"input": "$Country"}}
+        }
+    }
+
+    join = {
+        "$lookup": {
+            "from": "movies",
+            "localField": "country",
+            "foreignField": "country",
+            "as": "MovieInfo"
+        }
+    }
+
+    project = {
+        "$project": {"_id": 0, "country": 1, "GDP ($ per capita)": 1,
+                     "budgetRevenueRatio": {"$multiply": ["$budget", "$gross"]}},
+    }
+
+    joined_res = collection2.aggregate([
+        remove_whitespace,
+        join,
+    ])
+    # Process data to be scattered/analyzed
+    revGross = {}
+    gdps = []
+    for x in joined_res:
+        if x["MovieInfo"]:
+            total = 0
+            count = 0
+            for y in x["MovieInfo"]:
+                if (len(y["budget"]) != 0 and len(y["gross"]) != 0):
+                    temp = float(y["gross"]) / float(y["budget"])
+                    y["budgetGrossRatio"] = temp
+                    total += temp
+                    count += 1
+                print(y)
+            if count != 0:
+                avg = total / count
+                revGross[x["country"]] = avg
+                gdps.append(x["GDP ($ per capita)"])
+
+    print(revGross)
+    print(gdps)
+
+    figure = plt.figure(figsize=(15, 10))
+    plt.scatter( gdps, list(revGross.values()), color='blue')
+
+    plt.xlabel("GrossRevRatio")
+    plt.ylabel("GDP (per capita)")
+    plt.title("Average Gross Revenue Ratio vs GDP")
+    plt.show()
+
+
+
+"""
+Utilize MongoDB querying to access various collections from the entire database of movie information. Top genre per country map
+"""
+
+"""
+Utilize MongoDB querying to access various collections from the entire database of movie information.
+profit (gross - budget) vs score
+"""
 
 if __name__ == "__main__":
     client = pymongo.MongoClient(
-        "mongodb+srv://aryan2:password@cluster0.frmhm.mongodb.net/admin?retryWrites=true&w=majority")
+        "mongodb+srv://george:sujoysikdar@cluster0.frmhm.mongodb.net/admin?retryWrites=true&w=majority")
     db = client['movie_information']
-    collection = db["movies"]
+    collection1 = db["movies"]
+    collection2 = db["countries"]
+
+    # topPopularGenres(collection1)
+    budgetRevenueRelationship(collection1, collection2)
